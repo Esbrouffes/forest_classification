8213ea37da6ba9ae8b6894b5fad9ba335a027318
vnepveu <victor.nepveu@imt-atlantique.net>
2021-03-21 17:56:02 +0100

    Create MLP with 3 or 4 layers, remove dropout

--- mlp.py
+++ mlp.py
@@ -7,20 +7,23 @@ class MLP(nn.Module):
     Simple MLP for classification
     """
 
-    def __init__(self, input_dim, out_dim, layers, drop_rate):
+    def __init__(self, input_dim, out_dim, layers):
         super(MLP, self).__init__()
-        print(f"MLP architecture: {layers}")
         self.out_dim = out_dim
         self.input_dim = input_dim
+        self.n_layers = len(layers)
         self.layer1 = nn.Linear(input_dim, layers[0])
-        self.dropout1 = nn.Dropout(drop_rate)
         self.layer2 = nn.Linear(layers[0], layers[1])
         self.layer3 = nn.Linear(layers[1], layers[2])
-        self.layer_out = nn.Linear(layers[2], out_dim)
+        if self.n_layers > 3:
+            self.layer4 = nn.Linear(layers[2], layers[3])
+        self.layer_out = nn.Linear(layers[-1], out_dim)
+
     def forward(self, x):
         x = F.relu(self.layer1(x))
-        x = self.dropout1(x)
         x = F.relu(self.layer2(x))
         x = F.relu(self.layer3(x))
+        if self.n_layers > 3:
+            x = F.relu(self.layer4(x))
         x = self.layer_out(x)
         return x
