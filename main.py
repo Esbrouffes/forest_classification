cd2fe12c2fcb0b7ba4b8aa896c629bf500306bbf
vnepveu <victor.nepveu@imt-atlantique.net>
2021-03-21 18:20:54 +0100

    Put kaggle submission in another function
    - Also add command-line arguments for kaggle submission

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-import torch
-import pandas as pd
+import argparse
 
+from training import train, run_kaggle_submission
+from load_data import train_loader, val_loader, test_df, test_tensors
 import constants as CN
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", action="store_false")
+parser.add_argument("-n", default=100, type=int)
+parser.add_argument("--kaggle", action="store_true", default=False)
+args = parser.parse_args()
 
-def read_csv(filename: str) -> torch.Tensor:
-    df = pd.read_csv(filename)
-    return torch.Tensor(df.values)
+lr = 0.001
+n_epochs = args.n
+dropout_rate = 0.0
+regul = 0.00
+submit_to_kaggle = args.kaggle
+enable_tboard = args.t
+layers = [
+    # (1000, 500, 100, 50),
+    # (100, 500, 100, 50),
+    # (50, 100, 100, 50),
+    (500, 100, 10),
+    (50, 100, 10, 10),
+    # (400, 100, 50, 10),
+    # (1000, 500, 100, 50),
+    # (100, 100, 50, 20)
+]
+for layer in layers:
+    comment = f"Adam_{lr}_{layers}_{CN.BATCH_SIZE}_relu_regul_{regul}"
+    print(f"Model running : {comment}")
+    model = train(layer,
+                  lr=lr,
+                  n_epochs=n_epochs,
+                  regul=regul,
+                  train_loader=train_loader,
+                  val_loader=val_loader,
+                  enable_tboard=enable_tboard,
+                  comment=comment
+                  )
 
-
-train_data = read_csv(CN.TRAIN_FILE)
-test_data = read_csv(CN.TEST_FILE)
-print(train_data.shape)
-print(test_data.shape)
+    run_kaggle_submission(model,
+                          test_tensors=test_tensors,
+                          test_df=test_df,
+                          comment=comment,
+                          submit=True)
--- training.py
+++ training.py
@@ -1,39 +1,27 @@
-import argparse
 import os
-import time
 
 import torch
 import torch.nn as nn
 import tqdm
 from torch.utils.tensorboard import SummaryWriter
 
-from load_data import train_loader, val_loader, test_df, test_tensors
 from mlp import MLP
 import constants as CN
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", action="store_false")
-args = parser.parse_args()
 
-lr = 0.001
-n_epochs = 200
-dropout_rate = 0.0
-regul = 0.00
-
-
-def train(layers, lr, n_epochs, regul):
-    comment = f"Adam_{lr}_{layers}_{CN.BATCH_SIZE}_relu_regul_{regul}"
-    if not args.t:
+def train(layers, lr, n_epochs, regul, train_loader, val_loader, enable_tboard, comment):
+    if not enable_tboard:
         writer = SummaryWriter("runs/trash")
-        print(f"Running without logging")
+        print(f"Running without tensorboard logging")
     else:
+        print(f"Running with tensorboard logging")
         writer = SummaryWriter(comment=comment)
-        print(f"Running {comment}")
     model = MLP(CN.INPUT_DIM, CN.N_CLASS, layers=layers)
     print(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=regul)
     loss_function = nn.CrossEntropyLoss()
 
+    print(f"Training on {n_epochs} epochs")
     for epoch in tqdm.tqdm(range(n_epochs)):
         epoch_loss = 0
         model.train()
@@ -64,7 +52,12 @@ def train(layers, lr, n_epochs, regul):
         epoch_loss /= len(val_loader)
         writer.add_scalar("Accuracy", total_correct / total_sample, epoch)
         writer.add_scalar("Loss/val", epoch_loss, epoch)
+    writer.close()
+    return model
 
+
+def run_kaggle_submission(model, test_tensors, test_df, comment, submit=False):
+    print("Running with kaggle data")
     with torch.set_grad_enabled(False):
         model.eval()
         output = model(test_tensors)
@@ -73,22 +66,6 @@ def train(layers, lr, n_epochs, regul):
     output_classes += 1
     test_df["Cover_Type"] = output_classes
     test_df.to_csv("submission.csv", header=True, columns=["Id", "Cover_Type"], index=False)
-
-    os.system(CN.KAGGLE_COMMAND.format(comment))
-    # time.sleep(4)
-    # os.system(CN.KAGGLE_SUBMISSIONS)
-
-    writer.close()
-
-
-layers = [
-    # (1000, 500, 100, 50),
-    # (100, 500, 100, 50),
-    # (50, 100, 100, 50),
-    (500, 100, 10),
-    # (400, 100, 50, 10),
-    # (1000, 500, 100, 50),
-    # (100, 100, 50, 20)
-]
-for layer in layers:
-    train(layer)
+    if submit:
+        os.system(CN.KAGGLE_COMMAND.format(comment))
+        print("\n")
